6f34a771466bd2658f40228e96a4e07d453771e8
yoonju88 <yoonju.t@gmail.com>
2024-08-22 10:23:34 +0200

    added competenceList file

---
+++ frontend/src/containers/competenceList.js
@@ -0,0 +1,148 @@
+import HTML5 from '../images/icons/html5.png'
+import CSS3 from '../images/icons/css3.png'
+import JavaScript from '../images/icons/javascript.png'
+import React from '../images/icons/react.png'
+import Redux from '../images/icons/redux.png'
+import NodeJs from '../images/icons/nodeJs.png'
+import MongoDB from '../images/icons/mongoDB.png'
+import Sass from '../images/icons/sass.png'
+import GitHub from '../images/icons/gitHub.png'
+import VSC from '../images/icons/vsc.png'
+import Git from '../images/icons/git.png'
+import Swagger from '../images/icons/swagger.png'
+import Postman from '../images/icons/postman.png'
+import Figma from '../images/icons/figma.png'
+import Ps from '../images/icons/photoshop.png'
+import Ai from '../images/icons/illustrator.png'
+import Id from '../images/icons/indesign.png'
+import Ae from '../images/icons/aftereffects.png'
+
+
+export const competenceList = [
+     {
+        id: 'a1',
+        name : 'HTML5',
+        category :'Front-end',
+        image :   HTML5,
+        alt : 'Icône HTML5'
+    },
+    {
+        id: 'a2',
+        name : 'CSS3',
+        category :'Front-end',
+        image :   CSS3,
+        alt : 'Icône CSS3'
+    },
+    {
+        id: 'a3',
+        name : 'JavaScript',
+        category :'Front-end',
+        image :   JavaScript,
+        alt : 'Icône JavaScript'
+    },
+    {
+        id: 'a4',
+        name : 'React',
+        category :'Front-end',
+        image :   React,
+        alt : 'Icône React'
+    },
+    {
+        id: 'a5',
+        name : 'Redux',
+        category :'Front-end',
+        image :  Redux,
+        alt : 'Icône Redux'
+    },
+    {
+        id: 'a6',
+        name : 'Sass',
+        category :'Front-end',
+        image :  Sass,
+        alt : 'Icône Sass'
+    },
+    {
+        id: 'a7',
+        name : 'NodeJs',
+        category :'Back-end',
+        image :  NodeJs,
+        alt : 'Icône NodeJs'
+    },
+    {
+        id: 'a8',
+        name : 'MongoDB',
+        category :'Back-end',
+        image :  MongoDB,
+        alt : 'Icône MongoDB'
+    },
+    {
+        id: 'a9',
+        name : 'GitHub',
+        category :'Outils',
+        image :  GitHub,
+        alt : 'Icône GitHub'
+    },
+    {
+        id: 'a10',
+        name : 'Git',
+        category :'Outils',
+        image :  Git,
+        alt : 'Icône Git'
+    },
+    {
+        id: 'a11',
+        name : 'Visual Studio Code',
+        category :'Outils',
+        image :  VSC,
+        alt : 'Icône Visual Studio Code'
+    },
+    {
+        id: 'a12',
+        name : 'Swagger',
+        category :'Outils',
+        image :  Swagger,
+        alt : 'Icône Swagger'
+    },
+    {
+        id: 'a13',
+        name : 'Postman',
+        category :'Outils',
+        image :  Postman,
+        alt : 'Icône Postman'
+    },
+    {
+        id: 'a14',
+        name : 'Figma',
+        category :'Graphique',
+        image :  Figma,
+        alt : 'Icône Figma'
+    },
+    {
+        id: 'a15',
+        name : 'PhotoShop',
+        category :'Graphique',
+        image :  Ps,
+        alt : 'Icône PhotoShop'
+    },
+    {
+        id: 'a16',
+        name : 'Illustrator',
+        category :'Graphique',
+        image :  Ai,
+        alt : 'Icône Illustrator'
+    },
+    {
+        id: 'a17',
+        name : 'InDesign',
+        category :'Graphique',
+        image :  Id,
+        alt : 'Icône InDesign'
+    },
+    {
+        id: 'a18',
+        name : 'After Effects',
+        category :'Graphique',
+        image :  Ae,
+        alt : 'Icône After Effects'
+    },
+]
