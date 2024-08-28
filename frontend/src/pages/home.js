b875eeaa4d80e6d005a5ee7b7aa5e9e9011fe4ef
yoonju88 <yoonju.t@gmail.com>
2024-08-28 09:17:02 +0200

    restructured the code

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -2,20 +2,23 @@ import React from 'react'
 import Menu from '../components/Menu'
 import Footer from '../components/Footer'
 import './home.css'
+//import ProfileYJ from '../images/pages/yoonju.webp'
 
 
 function Home() {
     return (
-        <main className='home_page'>
+        <div className='home_page'>
             <Menu />
-            <div className='homeTxt_container'>
-                <h1>YOONJU <br/> TERRENOIRE</h1>
-                <p>DÉVELOPPEUSE WEB, FRONT END</p>
-            </div>
-            <Footer/>
-        </main>
-      
-        
+            <main>
+                <div className='homeTxt_container'>
+                    <h1>YOONJU <br /> TERRENOIRE</h1>
+                    <p>DÉVELOPPEUSE WEB, FRONT END</p>
+                </div>
+            </main>
+            <Footer />
+        </div>
+
+
     )
 }
 
