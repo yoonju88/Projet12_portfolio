e6d2ff5f74ca4064c1849cb55f11df503d257f5e
yoonju88 <yoonju.t@gmail.com>
2024-08-28 09:57:45 +0200

    Added style of modal + added setTimeout on this modal to close automatically

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,19 +1,33 @@
 import PropTypes from "prop-types"
 import closeIcon from '../images/icons/close.png'
-
+import React, {useEffect} from 'react'
 
 function Modal ({message, image, alt, onClick}) {
+
     const handleOverlayClick = (e) => {
         if (e.target === e.currentTarget){
             onClick()
-        }
+            }
     }
+    useEffect(() => {
+        const timer = setTimeout (() => {
+            onClick();
+        }, 3000)
+        return () => clearTimeout(timer)
+    }, [onClick])
     
     return (
-        <div className="modal-overlay" onClick={handleOverlayClick}>
+        <div className="modal_overlay" onClick={handleOverlayClick}>
             <div className='modal_container' onClick={e=> e.stopPropagation()}>
-                <img src={closeIcon} alt="close icon" onClick={onClick}/>
-                <p>{message}</p>
+                <span className="modal_close_icon">
+                    <img src={closeIcon} alt="close" onClick={onClick}/>
+                </span>
+                <div className="modal_content">
+                    <span className="modal_icon" >
+                        <img src={image} alt={alt}/>
+                    </span>                
+                    <p>{message}</p>
+                </div>
             </div>
         </div>
     )
