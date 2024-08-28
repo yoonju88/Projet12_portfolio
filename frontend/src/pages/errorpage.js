326e35bbd7d797a9e1e24e193aae575579b0b573
yoonju88 <yoonju.t@gmail.com>
2024-08-28 11:17:03 +0200

    added the sytle for error page

--- frontend/src/pages/errorpage.js
+++ frontend/src/pages/errorpage.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import Menu from '../components/Menu'
+import Footer from '../components/Footer'
+import './error.css'
 
 function NotFound (){
     return (
-        <div className='errorPage'>
-            <h1 className='errorPage_title'>404</h1>
-            <p className='errorPage_message'>Oups! La page que <br className='break'/> vous demnadez n'existe pas.</p>
-            <p className='errorPage_link'>
-                <Link to="/">
-                    Retourner sur la page d'accueil
-                </Link>
-            </p>
-        </div>
+        <>
+            <Menu />
+            <main className='error'>
+                <section className='error_container'>
+                    <h1>404</h1>
+                    <p className='errorPage_message'>La page que vous demnadez n'existe pas.</p>
+                    <span>
+                        <Link to="/" className='button'> Retour à l'acceuil </Link>
+                    </span>
+                </section>
+            </main>
+            <Footer />
+        </>
     )
 }
 export default NotFound
