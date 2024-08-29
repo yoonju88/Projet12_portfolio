79185248d5786d2289824f0fb95614494ac2e1d0
yoonju88 <yoonju.t@gmail.com>
2024-08-29 17:17:10 +0200

    correction the style sheet, removed height props

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -2,9 +2,6 @@ import React from 'react'
 import Menu from '../components/Menu'
 import Footer from '../components/Footer'
 import './home.css'
-import {Link} from 'react-router-dom'
-//import ProfileYJ from '../images/pages/yoonju.webp'
-
 
 function Home() {
     return (
@@ -15,13 +12,6 @@ function Home() {
                     <h1>YOONJU <br /> TERRENOIRE</h1>
                     <p>DÉVELOPPEUSE WEB, FRONT END</p>
                 </div>
-                <Link to="/about_me">
-                    <div className='arrow_container'>
-                        <span className="arrow"></span>
-                        <span className="arrow"></span>
-                        <span className="arrow"></span>
-                    </div>
-                </Link>
             </main>
             <Footer />
         </div>
