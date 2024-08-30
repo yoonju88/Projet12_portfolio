75c52922c89dab5a51e6b3e1a31bf83aa856105a
yoonju88 <yoonju.t@gmail.com>
2024-08-30 14:46:40 +0200

    update service item animation

--- frontend/src/components/ServiceItem.js
+++ frontend/src/components/ServiceItem.js
@@ -1,26 +1,50 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
 function ServiceItem ({icon, iconAlt, title, description}) {
     const [isOpened, setIsOpened] = useState(false)
+    const [itemHeight, setItemHeight] = useState('0')
+    const [opacity, setOpacity] =useState('0')
     const handleCollapse =(e) => {
         setIsOpened(!isOpened)
     }
+    useEffect(() => {
+        if (isOpened) {
+            setItemHeight ('285');
+            setOpacity('1')
+        } else if (!isOpened) {
+            setItemHeight('0')
+            setOpacity('0')
+        }
+    }, [isOpened])
+
+
     return (
         <li className="serviceItem" >
-            <div onClick={handleCollapse} className="serviceItem_title">
-                <div className="serviceIcon_container">
-                    <FontAwesomeIcon icon={icon}/>
-                </div> 
-                <h4 >{title}</h4>
-                { isOpened && (
-                    <div className={`description_box ${isOpened ? 'visible' : 'hidden'}`}>
-                        {description.map(text=>(
-                            <p key={text.id}>{text.text}</p>
-                        ))}
-                    </div>
-                )}                
+            <div 
+                onClick={handleCollapse} 
+                className="serviceItem_container"
+                style={{height: `${itemHeight}px`, transition: "height 0.3s ease"}}   
+            >
+                
+                <div className ="service-title">
+                    <div className="service_icon">
+                        <FontAwesomeIcon icon={icon}/>
+                    </div> 
+                    <h4 >{title}</h4>
+                </div>
+                {isOpened && (
+                    <div 
+                        className='description_box'
+                        style={{opacity: `${opacity}`, transition: "opacity 1s ease"}}
+                    >
+                    {description.map(text=>(
+                        <p key={text.id}>{text.text}</p>
+                    ))}
+                    </div>  
+                )}                    
             </div>
+            
         </li>
     )
 }
