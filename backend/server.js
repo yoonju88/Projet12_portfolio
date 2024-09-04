33054e5e552748d275a0c5cb8dcb5dab88abdbc3
yoonju88 <yoonju.t@gmail.com>
2024-09-04 21:46:50 +0200

    modifie port number

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.get('/health', (req, res) => {
     res.status(200).send('OK');
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
